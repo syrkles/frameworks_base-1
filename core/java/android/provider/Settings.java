dad8c6797e2c140d063b16a3e61c601960b92ff8
ezio84 <brabus84@gmail.com>
2018-11-04 16:09:22 +0100

    Stock navbar Pulse : allow to blacklist apps [1/2]
    useful for Whatsapp audio, Chrome videos or any other media
    we don't want Pulse for

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4966,6 +4966,12 @@ public final class Settings {
         public static final String PULSE_AUTO_COLOR = "pulse_auto_color";
 
         /**
+         * Apps to skip for Pulse
+         * @hide
+         */
+        public static final String PULSE_APPS_BLACKLIST = "pulse_apps_blacklist";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
